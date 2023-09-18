58379b05ae7e7e9cd6bd84ceb53d488d51bcc10c
Rhyss <rhyssjmn@gmail.com>
2023-09-19 00:47:34 +0800

    first commit

--- DSA/Linked List Implementation/Main.java
+++ DSA/Linked List Implementation/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+    static Scanner console = new Scanner(System.in);
     public static void main(String[] args) {
         IntSLList list1 = new IntSLList();
 
