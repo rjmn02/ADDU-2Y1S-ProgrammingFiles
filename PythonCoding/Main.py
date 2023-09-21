e19f615e88e6072234093eea2be8e9fdb0140b85
Rhyss <rhyssjmn@gmail.com>
2023-09-21 15:51:39 +0800

    9/21 - 3:51pm

--- PythonCoding/Main.py
+++ PythonCoding/Main.py
@@ -2,7 +2,18 @@
 
 def function():
     list = [5,2,2,6,7,1,2,5]
+    var = 5
+    var1 = 2
+    
+    for i in list:
+        for j in list:
+            temp = j[j+1]
+            j = j[j+1]
+            j[j+1] = temp 
+    
     for i in list:
         print(i)
+
+    
 
 function()
