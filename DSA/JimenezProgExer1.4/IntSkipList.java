e3faa153bdc875e46733f354e7c4b3e6b57c5b14
Rhyss <rhyssjmn@gmail.com>
2023-09-25 11:51:23 +0800

    larot

--- DSA/JimenezProgExer1.4/IntSkipList.java
+++ DSA/JimenezProgExer1.4/IntSkipList.java
@@ -15,4 +15,16 @@ public class IntSkipList {
             root[j] = null;
         choosePowers();
     }
+
+    public boolean isEmpty(){
+        return root[0] == null;
+    }
+
+    public void choosePowers(){
+        powers[maxLevel-1] = (2 << (maxLevel-1)) - 1;
+        for(int i = maxLevel -2, j = 0; i >= 0; i++, j++) {
+            powers[i] = powers[i+1] - (2 << j);
+        }
+    }
+    
 }
--- DSA/JimenezProgExer1.4/IntSkipListNode.java
+++ DSA/JimenezProgExer1.4/IntSkipListNode.java
@@ -1,3 +1,11 @@
 public class IntSkipListNode {
-    
+    public int key;
+    public IntSkipListNode[] next;
+    IntSkipListNode(int i, int n){
+        key = i;
+        next = new IntSkipListNode[n];
+        for(int j = 0; j < n; j++){
+            next[j] = null;
+        }
+    }
 }
