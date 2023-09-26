905f3896a07e8d8e1fd432d268a66716f10007e2
Rhyss <rhyssjmn@gmail.com>
2023-09-26 17:06:46 +0800

    laerot

--- DSA/JimenezProgExer1.4/UseSkipList.java
+++ DSA/JimenezProgExer1.4/UseSkipList.java
@@ -1,5 +1,23 @@
+import java.util.*;
+import java.io.*;
 public class UseSkipList {
     public static void main(String[] args) {
-        
+        writeToFile();
+    }
+
+    static void writeToFile(){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("data.txt"));
+            for(int i = 1; i <= 100; i++){
+                bw.write(String.valueOf(i));
+                bw.newLine();
+            }
+            bw.close();
+            System.out.println("Successfully wrote to the file.");
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
     }
 }
+
