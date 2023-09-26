fa0f69ecbcd04982dfeb9f309ea31fd7e78c6065
Rhyss <rhyssjmn@gmail.com>
2023-09-26 16:48:50 +0800

    larot

--- DSA/JimenezProgExer1.4/IntSkipList.java
+++ DSA/JimenezProgExer1.4/IntSkipList.java
@@ -21,9 +21,9 @@ public class IntSkipList {
     }
 
     public void choosePowers(){
-        powers[maxLevel-1] = (2 << (maxLevel-1)) - 1;
-        for(int i = maxLevel -2, j = 0; i >= 0; i++, j++) {
-            powers[i] = powers[i+1] - (2 << j);
+        powers[maxLevel-1] = (2 << (maxLevel-1)) - 1;   //2^maxlevel - 1
+        for(int i = maxLevel -2, j = 0; i >= 0; i--, j++) {
+            powers[i] = powers[i+1] - (2 << j);         //2^(j+1)
         }
     }
 
@@ -36,6 +36,79 @@ public class IntSkipList {
             }
         }
         return i - 1;
+    }
+
+    public int skipListSearch(int key){
+        int lvl;
+        IntSkipListNode prev, curr;
+        for(lvl = maxLevel - 1; lvl >= 0 && root[lvl] == null; lvl--);
+        prev = curr = root[lvl];
+        while(true) {
+            if(key == curr.key){
+                return curr.key;
+            }else if(key < curr.key){
+                if(lvl == 0){
+                    return 0;
+                }else if(curr == root[lvl]){
+                    curr = root[--lvl];
+                }else {
+                    curr = prev.next[--lvl];
+                }
+            }else{
+                prev = curr;
+                if(curr.next[lvl] != null){
+                    curr = curr.next[lvl];
+                }else{
+                    for(lvl--;lvl >= 0 && curr.next[lvl] == null; lvl--);
+                    if(lvl >= 0){
+                        curr = curr.next[lvl];
+                    }else {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void skipListInsert(int key){
+        IntSkipListNode[] curr = new IntSkipListNode[maxLevel];
+        IntSkipListNode[] prev = new IntSkipListNode[maxLevel];
+        IntSkipListNode newNode;
+
+        int lvl, i;
+        curr[maxLevel-1] = root[maxLevel-1];
+        prev[maxLevel-1] = null;
+        for(lvl = maxLevel - 1; lvl >= 0; lvl--){
+            while (curr[lvl] != null && curr[lvl].key < key) { // go to the next
+                prev[lvl] = curr[lvl]; // if smaller;
+                curr[lvl] = curr[lvl].next[lvl];
+            }
+            if (curr[lvl] != null && curr[lvl].key == key) // don't include
+                return; // duplicates;
+            if (lvl > 0) // go one level down
+                if (prev[lvl] == null) { // if not the lowest
+                    curr[lvl-1] = root[lvl-1]; // level, using a link
+                    prev[lvl-1] = null; // either from the root
+                }
+                else { // or from the predecessor;
+                    curr[lvl-1] = prev[lvl].next[lvl-1];
+                    prev[lvl-1] = prev[lvl];
+                }
+        }
+        lvl = chooseLevels();
+
+        newNode = new IntSkipListNode(key, lvl + 1);
+        for(i = 0;i <= lvl; i++){
+            newNode.next[i] = curr[i];
+            if(prev[i] == null){
+                root[i] = newNode;
+            }
+            else{
+                prev[i].next[i] = newNode;
+            }
+        }
+
     }
 
 
