23173ab07672d5fae8934d235e7726e4b8f87231
Rhyss <rhyssjmn@gmail.com>
2023-09-27 08:36:13 +0800

    SLL

--- DSA/JimenezProgExer1.5/SLLNode.java
+++ DSA/JimenezProgExer1.5/SLLNode.java
@@ -1,5 +1,15 @@
 
 
 public class SLLNode {
-    
+    public Student info;
+    public SLLNode next;
+
+    public SLLNode(Student info){
+        this(info, null);
+    } 
+
+    public SLLNode(Student info, SLLNode next){
+        this.info = info;
+        this.next = next;
+    }
 }
--- DSA/JimenezProgExer1.5/Student.java
+++ DSA/JimenezProgExer1.5/Student.java
@@ -1,4 +1,10 @@
 
 public class Student {
-    
+    private String lastName;
+    private String firstName;
+
+    public Student(String ln, String fn){
+        this.lastName = ln;
+        this.firstName = fn;
+    }
 }
