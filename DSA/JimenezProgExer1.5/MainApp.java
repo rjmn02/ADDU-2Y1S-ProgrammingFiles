8f55fc2d8e1eea1ba7335bc03c7a903ef913bebe
Rhyss <rhyssjmn@gmail.com>
2023-09-27 09:01:42 +0800

    vowels

--- DSA/JimenezProgExer1.5/MainApp.java
+++ DSA/JimenezProgExer1.5/MainApp.java
@@ -3,8 +3,31 @@ import java.io.*;
 public class MainApp {
     public static void main(String[] args) {
         LinkedList<Student> list = new LinkedList<>();
-        
+        readFile(list);
+    }
 
+    static void readFile(LinkedList<Student> list){
+        try {
+            FileReader fr = new FileReader("students.txt");
+            Scanner scan = new Scanner(fr);
+            while(scan.hasNextLine()){
+                String lastName = scan.nextLine();
+                String firstName = scan.nextLine();
+                
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    static boolean startsWithVowel(char[] lastName){
+        char firstLetter = lastName[lastName.length - 1];
+        if(firstLetter == 'a'|| firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' ||
+        firstLetter == 'u'){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
