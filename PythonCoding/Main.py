879b26a6103b1a896cbe0ca118d02ac17e1d02a1
Rhyss <rhyssjmn@gmail.com>
2023-10-01 16:16:27 +0800

    larot

--- PythonCoding/Main.py
+++ PythonCoding/Main.py
@@ -1,16 +1,11 @@
-
-
-def insertionSort():
-    list = [5,2,2,6,7,1,2,5]
-    for i in range(len(list)-1):
-        for j in range(len(list)-1, i, -1):
-            if list[j] < list[j-1]:
-                temp = list[j-1]
-                list[j-1] = list[j]
-                list[j] = temp 
-
-    for l in list:
-        print(l)
+class Node: 
+    info
+    Node next
     
+    def Node():
+        info = null
+        next = null
 
-insertionSort()
+    def Node(i, n):
+        info = i
+        next = n
