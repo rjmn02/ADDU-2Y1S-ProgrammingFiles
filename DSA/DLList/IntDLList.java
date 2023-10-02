21b2842e3ee9f89333a2b12481fa8f2d2c3601b1
Rhyss <rhyssjmn@gmail.com>
2023-10-02 11:34:14 +0800

    new folders/ doubly linked list implementation'

--- DSA/DLList/IntDLList.java
+++ DSA/DLList/IntDLList.java
@@ -11,14 +11,37 @@ public class IntDLList {
 
 
     //addtohead
-    public void addtohead(){
-        
+    public void addtohead(int val){
+        IntDLNode node = new IntDLNode(val);
+        if(isEmpty()){
+            head = tail = node;
+        }else{
+            head.prev = node;
+            node.next = head;
+            node.prev = null;
+            head = node;
+        }
     }
 
     //addtotail
-
-    //insert
-
+    public void addToTail(int val){
+        IntDLNode node = new IntDLNode(val);
+        if(isEmpty()){
+            head = tail = node;
+        }else{
+            tail.next = node;
+            node.prev = tail;
+            node.next = null;
+            tail = node;
+        }
+    }
     //display
+    public void display(){
+        IntDLNode temp;
+        for(temp = head; temp.next != null; temp = temp.next){
+            System.out.print(temp.info + " ");
+        }
+    }
     
 }
+
