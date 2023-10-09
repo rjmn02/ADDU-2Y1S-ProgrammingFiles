93d3bf8de33fd8f7226c6dc45279402ee4994815
Rhyss <rhyssjmn@gmail.com>
2023-10-09 12:34:20 +0800

    python

--- PythonCoding/Main.py
+++ PythonCoding/Main.py
@@ -1,4 +1,24 @@
-def larot():
-    print("larot")
+def insertionSort(arr): 
+    print("INSERTION SORT: ")
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > temp:
+            arr[j+1] = arr[j]
+            j = j - 1
 
-larot()
+        arr[j+1] = temp
+        print(arr)
+
+def bubbleSort(arr):
+    print("BUBBLESORT: ")
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1, 0, -1):
+            if arr[j] < arr[j-1]:
+                temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+    print(arr)
+
+arr = [6,2,3,1,5,73,9]
+bubbleSort(arr)
