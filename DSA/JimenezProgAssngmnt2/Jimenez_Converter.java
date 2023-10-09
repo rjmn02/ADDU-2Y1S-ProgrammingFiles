2b7fe851054896189ecb375bcec6b1ea9aa82426
Rhyss <rhyssjmn@gmail.com>
2023-10-09 09:35:25 +0800

    base 2 to 0

--- DSA/JimenezProgAssngmnt2/Jimenez_Converter.java
+++ DSA/JimenezProgAssngmnt2/Jimenez_Converter.java
@@ -35,35 +35,23 @@ public class Jimenez_Converter {
             System.out.println("BASE: ");
             if(console.hasNextInt()){
                 base = Integer.parseInt(console.nextLine());
-                if(base > 1 && base < 28)
+                if(base > 1 && base < 10)
                     break;
             }else{
                 console.nextLine();
-                System.out.println("Invalid Base. Must be between 2 and 27");
+                System.out.println("Invalid Base. Must be between 2 and 9.");
             }
         }
         return base;
     }
 
     static void decimalToBase(int base, int decimal, Stack<Object> stack){
-        if(base > 1 && base < 28){
+        if(base > 1 && base < 10){
             while(decimal != 0){
                 int remainder = (int) decimal%base;
-                String rmdrValue = getRmdrValue(Math.abs(remainder));
-
-                stack.push(rmdrValue);
+                stack.push(remainder);
                 decimal /= base;
             }
-        }
-    }
-
-    static String getRmdrValue(int r){
-        String letters = "ABCDEFGHIJKLMNOPQR";
-        char[] lettersArr = letters.toCharArray();
-        if(r > 9){
-            return String.valueOf(lettersArr[r-10]);
-        }else{
-            return String.valueOf(r);
         }
     }
 
