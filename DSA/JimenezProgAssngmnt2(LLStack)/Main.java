c6b15555742403346f6724a162eb6590640cf8b0
Rhyss <rhyssjmn@gmail.com>
2023-10-09 08:51:56 +0800

    ss

--- DSA/JimenezProgAssngmnt2(LLStack)/Main.java
+++ DSA/JimenezProgAssngmnt2(LLStack)/Main.java
@@ -1,20 +1,3 @@
-/*Write a program to convert a number from decimal notation to a number expressed
-in a number system whose base (or radix) is a number between 2 and 9. The conver-
-sion is performed by repetitious division by the base to which a number is being
-converted and then taking the remainders of division in the reverse order. 
-
-For example, in converting to binary, number 6 requires three such divisions:
-6/2 = 3 remainder 0, 
-3/2 = 1 remainder 1, 
-and finally, 1/2 = 0 remainder 1. 
-The remainders 0, 1, and 1 are put in the reverse order so that the binary equivalent of 6 is equal to 110.
-
-Modify your program so that it can perform a conversion in the case when the base
-is a number between 11 and 27. Number systems with bases greater than 10 require
-more symbols. Therefore, use capital letters. For example, a hexadecimal system
-requires 16 digits: 0, 1, . . ., 9, A, B, C, D, E, F. In this system, decimal number 26 is
-equal to 1A in hexadecimal notation, because 26/16 = 1 remainder 10 (that is, A),
-and 1/16 = 0 remainder 1. */
 import java.util.*;
 import java.io.*;
 
