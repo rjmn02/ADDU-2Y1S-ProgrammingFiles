4c17ba0e4e3d4d3261ea7a7a1d28cc386e436b00
Rhyss <rhyssjmn@gmail.com>
2023-10-14 12:07:55 +0800

    p

--- DSA/BST/UseBST.java
+++ DSA/BST/UseBST.java
@@ -1,29 +1,6 @@
-public class UseBST{
+
+public class UseBST {
     public static void main(String[] args) {
         
     }
-
-    static void depthFirstValues(){
-        BSTNode node1 = new BSTNode(0);
-        BSTNode node2 = new BSTNode(9);
-        BSTNode node3 = new BSTNode(5);
-        BSTNode node4 = new BSTNode(2);
-        BSTNode node5 = new BSTNode(7)
-        BSTNode node7 = new BSTNode(10);
-
-        node1.left = node2;
-        node1.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node4.le
-    }
-
-
-    /*
-                0
-               / \
-              9   5
-             / \
-            2   7
-     */
 }
