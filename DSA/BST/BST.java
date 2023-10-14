f938981982cc43f66ad58468dd4fdb2c9e2ec3b8
Rhyss <rhyssjmn@gmail.com>
2023-10-14 14:14:57 +0800

    BST implementation

--- DSA/BST/BST.java
+++ DSA/BST/BST.java
@@ -1,5 +1,50 @@
+import org.w3c.dom.Node;
+
 public class BST {
 
     protected BSTNode root;
 
+    public void insert(int key){
+        BSTNode node = new BSTNode(key);
+        this.root = insertHelper(this.root, node);
+    }
+    private BSTNode insertHelper(BSTNode root, BSTNode node){
+        if(root == null){
+            root = node;
+            return root;
+        }else if{root.data < node.data}{
+            return insertHelper(root.left, node);
+        }
+        return null;
+    }
+
+    public void remove(){
+
+    }
+    public void removeHelper(){}
+    public boolean search(int key){
+        return searchHelper(this.root, key) != null;
+    }
+    private BSTNode searchHelper(BSTNode root, int key){
+        if(root == null){
+            return null;
+        }else if(root.data == key){
+            return root;
+        }else if(root.data > key){
+            return searchHelper(root.left, key);
+        }else {
+            return searchHelper(root.right, key);
+        }
+    }
+
+    public void inorder(){
+        inorderHelper(this.root);
+    }
+    private void inorderHelper(BSTNode root) {
+        if (root != null) {
+            inorderHelper(root.left);
+            System.out.print(root.data + " ");
+            inorderHelper(root.right);
+        }
+    }
 }
--- DSA/BST/UseBST.java
+++ DSA/BST/UseBST.java
@@ -1,6 +1,18 @@
 
 public class UseBST {
     public static void main(String[] args) {
-        
+        BST tree = new BST();
+        tree.insert(35);
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(24);
+        tree.insert(58);
+        tree.insert(57);
+        tree.insert(65);
+        tree.insert(68);
+
+        System.out.println(tree.search(35));
+        System.out.println(tree.search(0));
+        tree.inorder();
     }
 }
