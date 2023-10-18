6f7c026990d488ffbc22efd5ded99460d5041df0
Rhyss <rhyssjmn@gmail.com>
2023-10-18 11:55:47 +0800

    1155am mysql

--- IM/dealership.sql
+++ IM/dealership.sql
@@ -11,43 +11,33 @@ Employee(
 
 CREATE TABLE IF NOT EXISTS
 Cashier(
+    CASHIER_ID INT NOT NULL auto_increment,
     EMPLOYEE_ID INT NOT NULL,
-    employee_lastname varchar(20),
-    employee_firstname varchar(20),
-    employee_phonenumber varchar(11),
 
-    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID),
-    FOREIGN KEY (employee_lastname) REFERENCES Employee(employee_lastname),
-    FOREIGN KEY (employee_firstname) REFERENCES Employee(employee_firstname),
-    FOREIGN KEY (employee_phonenumber) REFERENCES Employee(employee_phonenumber)
-    
+    PRIMARY KEY (CASHIER_ID),
+    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID)
 );
 
 CREATE TABLE IF NOT EXISTS
 Salesperson(
+    SALESPERSON_ID INT NOT NULL auto_increment,
     EMPLOYEE_ID INT NOT NULL,
-    employee_lastname varchar(20),
-    employee_firstname varchar(20),
-    employee_phonenumber varchar(11),
-    
 
-    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID),
-    FOREIGN KEY (employee_lastname) REFERENCES Employee(employee_lastname),
-    FOREIGN KEY (employee_firstname) REFERENCES Employee(employee_firstname),
-    FOREIGN KEY (employee_phonenumber) REFERENCES Employee(employee_phonenumber)
+    PRIMARY KEY (SALESPERSON_ID),
+    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID)
 );
 
 CREATE TABLE IF NOT EXISTS
 Invoice(
     INVOICE_ID INT NOT NULL auto_increment,
-    EMPLOYEE_ID INT NOT NULL,
+    SALESPERSON_ID INT NOT NULL,
     CUSTOMER_ID INT NOT NULL,
     SERIAL_NUMBER INT NOT NULL, 
     total_cost DOUBLE,
     date_issued DATE,
 
     PRIMARY KEY (INVOICE_ID),
-    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Salesperson(EMPLOYEE_ID),
+    FOREIGN KEY (SALESPERSON_ID) REFERENCES Salesperson(SALESPERSON_ID),
     FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID)
     FOREIGN KEY (SERIAL_NUMBER) REFERENCES Showroom_Car(SERIAL_NUMBER)
 );
@@ -66,7 +56,6 @@ CREATE TABLE IF NOT EXISTS
 Showroom_Car(
     SERIAL_NUMBER INT NOT NULL auto_increment,
     CUSTOMER_ID INT NOT NULL,
-    INVOICE_ID INT NOT NULL,
     brand varchar(20),
     model_year YEAR, 
     model varchar(20),
@@ -74,7 +63,6 @@ Showroom_Car(
     price DOUBLE,
     
     PRIMARY KEY (SERIAL_NUMBER),
-    FOREIGN KEY (INVOICE_ID) REFERENCES Invoice(INVOICE_ID),
     FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID)
 );
 
