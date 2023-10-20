6a482d9a54853ef6e75c2a2f29d04d934b0b9c7a
Rhyss <rhyssjmn@gmail.com>
2023-10-20 12:44:56 +0800

    10/20

--- IM/midterm.sql
+++ IM/midterm.sql
@@ -1,83 +1,135 @@
 CREATE TABLE IF NOT EXISTS
-Customer(
-    CUSTOMER_ID INT NOT NULL auto_increment, 
-    lastname varchar(20),
-    firstname varchar(20),
-    phonenumber varchar(11),
-    
-    PRIMARY KEY (CUSTOMER_ID)
-);
-
-CREATE TABLE IF NOT EXISTS
 Employee(
-    EMPLOYEE_ID INT NOT NULL auto_increment,
-    employee_lastname varchar(20),
-    employee_firstname varchar(20),
-    employee_phonenumber varchar(11),
+	EMPLOYEE_ID INT NOT NULL auto_increment,
+	employee_lastname varchar(20),
+	employee_firstname varchar(20),
+	employee_phonenumber varchar(11),
 
-    PRIMARY KEY(EMPLOYEE_ID)
+	PRIMARY KEY(EMPLOYEE_ID)
 );
 
 CREATE TABLE IF NOT EXISTS
 Salesperson(
-    EMPLOYEE_ID INT NOT NULL,
-    SALES_RECORD_ID INT NOT NULL,
-    employee_lastname varchar(20),
-    employee_firstname varchar(20),
-    employee_phonenumber varchar(11),
-    
+	SALESPERSON_ID INT NOT NULL auto_increment,
+	EMPLOYEE_ID INT NOT NULL,
 
-    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID),
-    UNIQUE KEY (SALES_RECORD_ID),
-    FOREIGN KEY (employee_lastname) REFERENCES Employee(employee_lastname),
-    FOREIGN KEY (employee_firstname) REFERENCES Employee(employee_firstname),
-    FOREIGN KEY (employee_phonenumber) REFERENCES Employee(employee_phonenumber),
+	PRIMARY KEY (SALESPERSON_ID),
+	FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID)
 );
 
 CREATE TABLE IF NOT EXISTS
-Sales_Record(
-    SALES_RECORD_ID INT NOT NULL auto_increment,
-    EMPLOYEE_ID INT NOT NULL,
-    SERIAL_NUMBER INT NOT NULL,
-    INVOICE_ID INT NOT NULL,
-    CUSTOMER_ID INT NOT NULL,
+Customer(
+	CUSTOMER_ID INT NOT NULL auto_increment,
+	lastname varchar(20),
+	firstname varchar(20),
+	phonenumber varchar(11),
 
-    PRIMARY KEY (SALES_RECORD_ID),
-    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Salesperson(EMPLOYEE_ID),
-    FOREIGN KEY (SERIAL_NUMBER) REFERENCES Car(SERIAL_NUMBER),
-    FOREIGN KEY (INVOICE_ID) REFERENCES Invoice(INVOICE_ID),
-    FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID),
-    FOREIGN KEY (employee_lastname) REFERENCES Salesperson(employee_lastname),
-    FOREIGN KEY (employee_firstname) REFERENCES Salesperson(employee_firstname),
-    FOREIGN KEY (employee_phonenumber) REFERENCES Salesperson(employee_phonenumber),
+	PRIMARY KEY (CUSTOMER_ID)
 );
 
 CREATE TABLE IF NOT EXISTS
-Invoice(
-    INVOICE_ID INT NOT NULL auto_increment,
-    SERIAL_NUMBER INT NOT NULL,
-    EMPLOYEE_ID INT NOT NULL,
-    CUSTOMER_ID INT NOT NULL,
-    total_cost DOUBLE,
-    date_issued DATE,
+Showroom_Car(
+SERIAL_NUMBER INT NOT NULL,
+CUSTOMER_ID INT,
 
-    PRIMARY KEY (INVOICE_ID),
-    FOREIGN KEY (SERIAL_NUMBER) REFERENCES Car(SERIAL_NUMBER),
-    FOREIGN KEY (EMPLOYEE_ID) REFERENCES Salesperson(EMPLOYEE_ID),
-    FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID)
+	FOREIGN KEY(SERIAL_NUMBER) REFERENCES Car(SERIAL_NUMBER),
+	FOREIGN KEY(CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Customer_Car(
+SERIAL_NUMBER INT NOT NULL,
+CUSTOMER_ID INT,
+SERVICE_HISTORY_ID INT NOT NULL,
+
+	FOREIGN KEY(SERIAL_NUMBER) REFERENCES Car(SERIAL_NUMBER),
+	FOREIGN KEY(CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID),
+	FOREIGN KEY(SERVICE_HISTORY_ID) REFERENCES Service_History(SERVICE_HISTORY_ID)
 );
 
 CREATE TABLE IF NOT EXISTS
 Car(
-    SERIAL_NUMBER INT NOT NULL auto_increment,
-    CUSTOMER_ID INT NOT NULL,
-    INVOICE_ID INT NOT NULL,
-    brand varchar(20),
-    model_year YEAR, 
-    model varchar(20),
-    variant varchar(20),
-    price DOUBLE,
-    PRIMARY KEY (SERIAL_NUMBER),
-    FOREIGN KEY (INVOICE_ID) REFERENCES Invoice(INVOICE_ID),
-    FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID)
+	SERIAL_NUMBER INT NOT NULL auto_increment,
+	CUSTOMER_ID INT NOT NULL,
+	brand varchar(20),
+	model_year YEAR,
+	model varchar(20),
+	variant varchar(20),
+	price DOUBLE,
+	transmission varchar(2),
+    
+	PRIMARY KEY (SERIAL_NUMBER),
+	FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Invoice(
+	INVOICE_ID INT NOT NULL auto_increment,
+	EMPLOYEE_ID INT NOT NULL,
+	CUSTOMER_ID INT,
+	SERIAL_NUMBER INT,
+	total_cost DOUBLE,
+	date_issued DATE,
+
+	PRIMARY KEY (INVOICE_ID),
+	FOREIGN KEY (EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID),
+	FOREIGN KEY (CUSTOMER_ID) REFERENCES Customer(CUSTOMER_ID),
+	FOREIGN KEY (SERIAL_NUMBER) REFERENCES Showroom_Car(SERIAL_NUMBER)
+);
+
+CREATE TABLE IF NOT EXISTS
+Mechanic(
+	MECHANIC_ID INT NOT NULL auto_increment,
+	EMPLOYEE_ID INT NOT NULL,
+	
+	PRIMARY KEY(MECHANIC_ID),
+	FOREIGN KEY(EMPLOYEE_ID) REFERENCES Employee(EMPLOYEE_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Service(
+	SERVICE_ID INT NOT NULL auto_increment,
+	MECHANIC_ID INT NOT NULL,
+	service_type varchar(20),
+
+PRIMARY KEY(SERVICE_ID),
+FOREIGN KEY(MECHANIC_ID) REFERENCES Mechanic(MECHANIC_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Service_History(
+	SERVICE_HISTORY_ID INT NOT NULL auto_increment,
+	SERIAL_NUMBER INT NOT NULL,
+	SERVICE_ID INT NOT NULL,
+
+	PRIMARY KEY(SERVICE_HISTORY_ID),
+	FOREIGN KEY(SERIAL_NUMBER) REFERENCES Car(SERIAL_NUMBER),
+	FOREIGN KEY(SERVICE_ID) REFERENCES Service(SERVICE_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Maintenance(
+	SERVICE_ID INT NOT NULL,
+	Maintenance_description varchar(50),
+
+	FOREIGN KEY(SERVICE_ID) REFERENCES Service(SERVICE_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Part(
+	PART_ID INT NOT NULL auto_increment,
+	part_name varchar(20),
+	price DOUBLE,
+
+	PRIMARY KEY(PART_ID)
+);
+
+CREATE TABLE IF NOT EXISTS
+Part_Replacement_Summary(
+	SERVICE_ID INT NOT NULL,
+	PART_ID INT NOT NULL,
+	part_quantity INT NOT NULL,
+
+	FOREIGN KEY(SERVICE_ID) REFERENCES Service(SERVICE_ID),
+	FOREIGN KEY(PART_ID) REFERENCES Part(PART_ID)
 );
