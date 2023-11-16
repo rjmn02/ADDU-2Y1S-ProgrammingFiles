f6d271e19a2795b243d156d56d6eedc405cb4108
Rhyss <rhyssjmn@gmail.com>
2023-11-17 02:13:14 +0800

    ww

--- DSA/JimenezProgExer1.9/Software.java
+++ DSA/JimenezProgExer1.9/Software.java
@@ -5,4 +5,55 @@ public class Software {
     private int quantity;
     private double price;
 
+    public Software(String name, String version, int quantity, double price) {
+        this.name = name;
+        this.version = version;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
 }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " name='" + getName() + "'" +
+            ", version='" + getVersion() + "'" +
+            ", quantity='" + getQuantity() + "'" +
+            ", price='" + getPrice() + "'" +
+            "}";
+    }
---
+++ DSA/JimenezProgExer1.9/SoftwareBST.java
@@ -0,0 +1,98 @@
+public class SoftwareBST {
+    
+    private Node root;
+
+    static CompareSoftware comp = new CompareSoftware();
+    //insert
+    public void insertSoftware(Software key){
+        this.root = insertRec(this.root, new Node(key));
+    }
+
+    private Node insertRec(Node root, Node key){
+        if(root == null){
+            root = key;
+            return root;
+        } else if(comp.compare(root.data, key.data) == 0){
+            //update quantity
+
+        } else if(comp.compare(root.data, key.data) < 0){
+            root = insertRec(root.right, key);
+        } else {
+            root = insertRec(root.left, key);
+        }
+
+        return root;
+    }
+
+    //search
+    public boolean search(Software key){
+        return searchRec(this.root, key) != null;
+    }
+
+    private Node searchRec(Node root, Software key){
+        
+        if(comp.compare(root.data, key) == 0){
+            return root;
+        } else if(comp.compare(root.data, key) < 0){
+            searchRec(root.right, key);
+        } else {
+            searchRec(root.left, key);
+        }
+        return root;
+    }
+
+    //delete
+    public void delete(Software key){
+        if (!search(key))
+            this.root = deleteByCopy(this.root, key);
+    }
+    
+    private Node deleteByCopy(Node root, Software key) {
+        if(comp.compare(root.data, key) == 0){
+            //leaf
+            if(root.left == null && root.right == null){
+                //update quant
+                return root;
+            }
+            //1 child
+            else if(root.left == null || root.right == null){
+                Node temp = root;
+                root = (root.left != null) ? deleteByCopy(root.left, key) : deleteByCopy(root.right, key);
+
+                return root;
+            }
+            //2 children
+            else{
+                Node temp = findMin(root.left);
+                root.data = temp.data;
+                root.left = deleteByCopy(temp, key);
+            }
+
+
+            return root;
+        } else if(comp.compare(root.data, key) < 0){
+            root = deleteByCopy(root.right, key);
+        } else {
+            root = deleteByCopy(root.left, key);
+        }
+        return root;
+    }
+
+    //findmin
+    private Node findMin(Node p){
+        while(p.right != null)
+            p = p.right;
+
+        return p;
+    }
+
+
+    //update quantity
+    private void updateQuantity(Node root, Node key){
+        if(comp.compare(root.data, key.data) == 0){
+            root.data.setQuantity(key.data.getQuantity());
+        }else if(root.data.getQuantity())
+    }
+
+
+}
