f272fb41207f102b984c92f93c830b851caf4cc4
Rhyss <rhyssjmn@gmail.com>
2023-11-21 00:37:42 +0800

    remove white space

--- DSA/Graph Implementation/Main.java
+++ DSA/Graph Implementation/Main.java
@@ -16,20 +16,14 @@ public class Main {
             
             String line;
             while ((line = br.readLine()) != null) {
-
                 String[] verts = line.split(" ");
-
                 if(verts.length == 1){
                     graph.addVertex(new Vertex(verts[0].charAt(0)));   
-
                 }else if(verts.length == 2){
-
                     //get vertex from graph
                     //add edges 
-
                     Vertex v1 = graph.getVertex(verts[0].charAt(0));
                     Vertex v2 = graph.getVertex(verts[1].charAt(0));
-
                     if(v1 != null && v2 != null){
                         graph.addEdge(v1, v2);
                     }
