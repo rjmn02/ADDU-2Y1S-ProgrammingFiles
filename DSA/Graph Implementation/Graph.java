66fc804f115270d27d3c4bb2cc5a133cba491039
Rhyss <rhyssjmn@gmail.com>
2023-11-20 13:16:46 +0800

    jakoll

---
+++ DSA/Graph Implementation/Graph.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Graph{
+
+    ArrayList<LinkedList<Vertex>> alist = new ArrayList<>();
+
+
+}
---
+++ DSA/Graph Implementation/Vertex.java
@@ -0,0 +1,7 @@
+public class Vertex{
+    protected Object data;
+
+    public Vertex(Object data){
+        this.data = data;
+    }
+}
