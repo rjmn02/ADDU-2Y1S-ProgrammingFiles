601d4335de4eeed3e42538362c0a2898b3f47a94
Rhyss <rhyssjmn@gmail.com>
2023-11-27 20:07:52 +0800

    prefi code systems tables

--- IM/prefi.sql
+++ IM/prefi.sql
@@ -1,37 +1,73 @@
-CREATE TABLE IF NOT EXISTS 
-payment_methods(
+CREATE TABLE IF NOT EXISTS
+system_components(
+    id INT NOT NULL,
+    PRIMARY KEY(id),
+    component_id INT NOT NULL,
+    system_id INT NOT NULL,
+
+
+)
+
+system_team_choices(
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id),
-    airtable_id VARCHAR(100),
-    issuing_bank VARCHAR(200),
-    cardholder_name VARCHAR(200),
-    card_last_four_digits VARCHAR(4)
+    description VARCHAR(30),
+),
+
+system_audit_statuses(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    description VARCHAR(30),
+),
+
+system_audit_frequencies(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    description VARCHAR(30),
+),
+
+system_statuses(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    description VARCHAR(30),
+),
+
+systems_approvers(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
 
 ),
 
-application_payment_methods(),
-
-it_team_member_statuses(
+systems(
     id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
-    description VARCHAR(200),
-),
-
-it_team_member_role(
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id),
-    description VARCHAR(200),
-),
-
-it_team_member(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    airtable_id VARCHAR(100) NOT NULL,
-    name VARCHAR(75) NOT NULL,
+    PRIMARY KEY(ID),
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(500),
+    service_start DATE NOT NULL,
+    service_end DATE,
     status_id INT NOT NULL,
-    FOREIGN KEY (status_id) REFERENCES it_team_member_statuses(id),
-    email_address VARCHAR(500) NOT NULL,
-    slack_member_id VARCHAR(200) NOT NULL,
-    asana_user_id VARCHAR(200) NOT NULL,
-    it_team_member_role_id INT NOT NULL,
-    FOREIGN KEY (it_team_member_role_id) REFERENCES it_team_member_role(id)
+    FOREIGN KEY(status_id) REFERENCES system_statuses(id),
+    audit_frequency_id INT NOT NULL,
+    FOREIGN KEY(audit_frequency_id) REFERENCES system_audit_frequencies(id),
+    last_id_audit DATE,
+    next_id_audit DATE NOT NULL,
+    --approver_id INT NOT NULL,
+    --FOREIGN KEY(approver_id) REFERENCES,
+    team_id INT NOT NULL,
+    FOREIGN KEY(team_id) REFERENCES system_team_choices(id),
+    video_sop_link VARCHAR(200),
+    written_sop_link VARCHAR(200),
+    flowchart_link VARCHAR(200),
+    audit_status_id INT NOT NULL,
+    FOREIGN KEY(audit_status_id) REFERENCES system_audit_statuses(id)
 ),
+
+system_teams(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    team_id INT NOT NULL,
+    FOREIGN KEY(team_id) REFERENCES system_team_choices(id),
+    system_id INT NOT NULL,
+    FOREIGN KEY(system_id) REFERENCES systems(id)
+),
+
